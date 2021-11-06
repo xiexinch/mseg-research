d50ea4934a777c1197c3cca610005fbb53873635
xiexinch <xinchen.xie@qq.com>
2021-11-06 16:44:19 +0800

    add 2x2 config

---
+++ configs/bisenet_hybrid/bisenet_hybrid_vit_2x2_512x1024_20k_cityscapes_0125.py
@@ -0,0 +1,11 @@
+_base_ = [
+    '../_base_/models/bisenet_hybrid.py', './cityscapes_0125.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
+]
+
+lr_config = dict(warmup='linear', warmup_iters=1000)
+optimizer = dict(lr=0.025)
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=4,
+)
