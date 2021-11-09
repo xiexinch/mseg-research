a699a30c8daf5dc4ca55bab630200f62de58ccf6
xiexinch <test767803@foxmail.com>
2021-11-09 15:45:09 +0800

    reset dataset

--- configs/bisenet_efficienet_baseline/bisenetv1_efficientnetv2-s_in21k-pre_2x4_512x1024_20k_cityscapes0125.py
+++ configs/bisenet_efficienet_baseline/bisenetv1_efficientnetv2-s_in21k-pre_2x4_512x1024_20k_cityscapes0125.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/models/bisenetv1_r18-d32.py',
-    './cityscapes_0125.py',
+    '../_base_/datasets/cityscapes_1024x1024.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
 ]
 model = dict(
