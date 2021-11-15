fcda4948d34391825025046a481ec59da1262208
谢昕辰 <xinchen.xie@qq.com>
2021-11-16 00:34:24 +0800

    add pspnet mobilenet configs

---
+++ configs/mobilenet_v2/pspnet_mobilenet_v2-d32_2x4_160k_512x1024_cityscapes.py
@@ -0,0 +1,17 @@
+_base_ = [
+    '../_base_/models/pspnet_mobilenet_v2-d32.py',
+    # './cityscapes_0125.py',
+    '../_base_/datasets/cityscapes.py',
+    '../_base_/default_runtime.py',
+    '../_base_/schedules/schedule_160k.py'
+]
+
+model = dict(backbone=dict(init_cfg=None))
+
+lr_config = dict(warmup='linear', warmup_iters=1000)
+# optimizer = dict(lr=0.025)
+
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=4,
+)
