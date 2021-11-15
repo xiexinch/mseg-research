012acbbac87c1bb0d913c10239de927e52b6840f
xiexinch <test767803@foxmail.com>
2021-11-15 13:18:07 +0800

    new exp

---
+++ configs/fastscnn/fast_scnnexp_attn_lr0.25_2x4_20k_cityscapes0125.py
@@ -0,0 +1,16 @@
+_base_ = [
+    '../_base_/models/fast_scnn.py', '../_base_/datasets/cityscapes.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
+]
+
+model = dict(
+    backbone=dict(
+        type='FastSCNNEXP'
+    )
+)
+
+# Re-config the data sampler.
+data = dict(samples_per_gpu=4, workers_per_gpu=4)
+
+# Re-config the optimizer.
+optimizer = dict(type='SGD', lr=0.25, momentum=0.9, weight_decay=4e-5)
