c078ffd878e27e7cd9324d9fadc9a0ce22080ec7
xiexinch <test767803@foxmail.com>
2021-11-18 15:08:29 +0800

    conv exp

---
+++ configs/mobilenet_v2/ffm_noconcat_conv1_mobilenet_v2-d32_inpre_2x4_20k_512x1024.py
@@ -0,0 +1,17 @@
+_base_ = [
+    '../_base_/models/ffm_mobilenetv2-d32.py', './cityscapes_0125.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
+]
+
+model = dict(decode_head=dict(
+    num_convs=1,
+    concat_input=False
+))
+
+lr_config = dict(warmup='linear', warmup_iters=1000)
+# optimizer = dict(lr=0.025)
+
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=4,
+)
