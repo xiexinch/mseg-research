2ea88d0a973b5ad5bce841efae9ef369ea96e9a5
xiexinch <test767803@foxmail.com>
2021-11-21 16:36:36 +0800

    add fccmext multi

---
+++ configs/fccm/fccmext_mobilenet_v2-d32_inpre_2x4_20k_512x1024_cityscapes_multi_auxiliary.py
@@ -0,0 +1,66 @@
+_base_ = [
+    '../_base_/models/fccm_mobilenetv2-d32.py', '../_base_/datasets/cityscapes.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
+]
+
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+model = dict(
+    decode_head=dict(type='FCCMHead_EXT'),
+    auxiliary_head=[
+        dict(
+            type='FCNHead',
+            in_channels=24,
+            channels=24,
+            num_convs=2,
+            num_classes=19,
+            in_index=0,
+            norm_cfg=norm_cfg,
+            concat_input=False,
+            align_corners=False,
+            loss_decode=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+        dict(
+            type='FCNHead',
+            in_channels=32,
+            channels=64,
+            num_convs=2,
+            num_classes=19,
+            in_index=1,
+            norm_cfg=norm_cfg,
+            concat_input=False,
+            align_corners=False,
+            loss_decode=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+        dict(
+            type='FCNHead',
+            in_channels=96,
+            channels=256,
+            num_convs=2,
+            num_classes=19,
+            in_index=2,
+            norm_cfg=norm_cfg,
+            concat_input=False,
+            align_corners=False,
+            loss_decode=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+        dict(
+            type='FCNHead',
+            in_channels=320,
+            channels=512,
+            num_convs=2,
+            num_classes=19,
+            in_index=3,
+            norm_cfg=norm_cfg,
+            concat_input=False,
+            align_corners=False,
+            loss_decode=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    ])
+
+lr_config = dict(warmup='linear', warmup_iters=1000)
+# optimizer = dict(lr=0.025)
+
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=4,
+)
